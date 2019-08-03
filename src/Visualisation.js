74b5a3ab84181bfcc7cde8fefdaa57632e9f0e92
Robert Chang <cht8687@gmail.com>
2019-08-03 20:14:05 +1000

    color update

--- src/Visualisation.js
+++ src/Visualisation.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
+import getRandomColor from './utils/getRandomColor';
 import './Visualisation.css';
 
 function Visualisation(props) {
   const visEl = useRef(null);
   useEffect(() => {
     const svgContainer = d3.select(visEl.current);
+    const colorValue = getRandomColor();
 
     const jsonRectangles = [
       {
@@ -13,48 +15,51 @@ function Visualisation(props) {
         y_axis: 10,
         height: 50,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.3)',
       },
       {
         x_axis: 10,
         y_axis: 60,
         height: 50,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.4)',
       },
       {
         x_axis: 10,
         y_axis: 110,
         height: 100,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.5)',
       },
       {
         x_axis: 10,
         y_axis: 210,
         height: 100,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.6)',
       },
       {
         x_axis: 10,
         y_axis: 310,
         height: 140,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.7)',
       },
       {
         x_axis: 10,
         y_axis: 450,
         height: 140,
         width: '100%',
-        color: 'rgba(60, 186, 84, 0.8)',
       },
     ];
 
+    const formatedData = jsonRectangles.map((rec, index) => {
+      const opacity = (index * 10 + 20) / 100 > 1 ? 1 : (index * 10 + 20) / 100;
+      return {
+        ...rec,
+        opacity,
+        color: colorValue,
+      };
+    });
+
     const rectangles = svgContainer
       .selectAll('rect')
-      .data(jsonRectangles)
+      .data(formatedData)
       .enter()
       .append('rect');
 
@@ -73,6 +78,9 @@ function Visualisation(props) {
       })
       .style('fill', function(d) {
         return d.color;
+      })
+      .style('opacity', function(d) {
+        return d.opacity;
       });
   }, props.data);
 
