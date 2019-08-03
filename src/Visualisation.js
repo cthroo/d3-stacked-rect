5922a23ed8da74871f7233a7ed5e08780a726548
Robert Chang <cht8687@gmail.com>
2019-08-03 19:08:43 +1000

    Agument CSS

--- src/Visualisation.js
+++ src/Visualisation.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import * as d3 from 'd3';
+import './Visualisation.css';
 
 function Visualisation(props) {
   const visEl = useRef(null);
@@ -7,12 +8,48 @@ function Visualisation(props) {
     const svgContainer = d3.select(visEl.current);
 
     const jsonRectangles = [
-      {x_axis: 10, y_axis: 10, height: 50, width: 200, color: 'green'},
-      {x_axis: 10, y_axis: 60, height: 50, width: 200, color: 'purple'},
-      {x_axis: 10, y_axis: 110, height: 100, width: 200, color: 'red'},
-      {x_axis: 10, y_axis: 210, height: 100, width: 200, color: 'blue'},
-      {x_axis: 10, y_axis: 310, height: 140, width: 200, color: 'pink'},
-      {x_axis: 10, y_axis: 450, height: 140, width: 200, color: 'silver'},
+      {
+        x_axis: 10,
+        y_axis: 10,
+        height: 50,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.3)',
+      },
+      {
+        x_axis: 10,
+        y_axis: 60,
+        height: 50,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.4)',
+      },
+      {
+        x_axis: 10,
+        y_axis: 110,
+        height: 100,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.5)',
+      },
+      {
+        x_axis: 10,
+        y_axis: 210,
+        height: 100,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.6)',
+      },
+      {
+        x_axis: 10,
+        y_axis: 310,
+        height: 140,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.7)',
+      },
+      {
+        x_axis: 10,
+        y_axis: 450,
+        height: 140,
+        width: '100%',
+        color: 'rgba(60, 186, 84, 0.8)',
+      },
     ];
 
     const rectangles = svgContainer
@@ -41,9 +78,11 @@ function Visualisation(props) {
 
   return (
     <>
-      <svg height="600" width="500">
-        <g ref={visEl} />
-      </svg>
+      <div className="svg-container">
+        <svg height="500">
+          <g ref={visEl} />
+        </svg>
+      </div>
     </>
   );
 }
