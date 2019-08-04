a7b927180bd047f8e5ebf90b67c7d72df77a1212
Robert Chang <cht8687@gmail.com>
2019-08-04 21:19:44 +1000

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # d3-stacked-rect
+
+<big><h1 align="center">d3 stacked rect component based on their weights</h1></big>
+
+![d3 stacked rect](example/example.png)
+
+## Installation
+
+### npm
+
+```
+$ npm install --save d3-stacked-react
+```
+
+## Usage
+
+Below is the example of custom components:
+
+```js
+<D3StackedRect data={data} height={700} />
+```
+
+## Options
+
+#### `data`: PropTypes.object.isRequired
+
+```js
+const data = {
+  titles: {
+    P3: 10,
+    P4: 12,
+    P5: 13,
+    P6: 8,
+    P7: 3,
+    P8: 3,
+    P9: 2,
+  },
+  subTitles: {
+    P3: 'Junior Engineer',
+    P4: 'Engineer',
+    P5: 'Senior Engineer',
+    P6: '',
+    P7: 'Senior Principle Engineer',
+    P8: 'Lead Principle Engineer',
+    P9: 'Distinguised Engineer',
+  },
+};
+```
+
+Note: if associated title field in subTitles is empty, it won't render the subTitles. Otherwise, it will add '-' to the subTitles.
+
+## height
+
+#### `height`: PropTypes.number.isRequired
+
+Set the height of of the SVG.
+
+## License
+
+MIT
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-import Visualisation from './Visualisation';
+import React, {useEffect, useRef} from 'react';
+import {head, keys, values, concat} from 'ramda';
+import * as d3 from 'd3';
+import getRandomColor from './utils/getRandomColor';
+import generatedata from './utils/generateData';
+
+function Visualisation(props) {
+  const visEl = useRef(null);
+  useEffect(() => {
+    const svgContainer = d3.select(visEl.current);
+    const colorValue = getRandomColor();
+    const rectData = generatedata(props.data);
+    const formatedData = rectData.map((rec, index) => {
+      const opacity = (index * 10 + 20) / 100 > 1 ? 1 : (index * 10 + 20) / 100;
+      return {
+        ...rec,
+        opacity,
+        color: colorValue,
+      };
+    });
+
+    const container = svgContainer
+      .selectAll('g')
+      .data(formatedData)
+      .enter()
+      .append('g');
+
+    container
+      .append('rect')
+      .attr('x', d => d.x)
+      .attr('y', d => d.y)
+      .attr('height', d => d.height)
+      .attr('width', d => d.width)
+      .attr('class', () => 'rect-style')
+      .style('fill', d => d.color)
+      .style('opacity', d => d.opacity);
+
+    container
+      .append('text')
+      .attr('x', d => d.x)
+      .attr('y', d => d.y + d.height / 2)
+      .style('font-size', 18)
+      .attr('dy', '.35em')
+      .text(d =>
+        head(values(d.title))
+          ? concat(head(keys(d.title)), `-${head(values(d.title))}`)
+          : head(keys(d.title)),
+      )
+      .attr('transform', d => 'translate(' + 10 + ',' + 0 + ')');
+  }, props.data);
+
+  return (
+    <svg height={props.height}>
+      <g ref={visEl} />
+    </svg>
+  );
+}
 
 export default Visualisation;
